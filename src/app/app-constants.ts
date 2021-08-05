4c183c242c572f6863392f4a4cdcec49ec51fae4
andersonsouza90 <andersonsilva.souza90@gmail.com>
2021-08-05 00:57:16 -0300

    appconstants

--- src/app/app-constants.ts
+++ src/app/app-constants.ts
@@ -1,7 +1,7 @@
 export class AppConstants {
 
-	public static get baseServidor(): string{ return "http://localhost:8080/" }
-  //public static get baseServidor(): string{ return "https://chatbot-argo.herokuapp.com/" }
+	//public static get baseServidor(): string{ return "http://localhost:8080/" }
+  public static get baseServidor(): string{ return "https://chatbot-argo.herokuapp.com/" }
 	public static get baseUrl(): string{ return this.baseServidor + "chatbot/question/"}
   public static get baseUrlAnswer(): string{ return this.baseServidor + "chatbot/answer/"}
 
